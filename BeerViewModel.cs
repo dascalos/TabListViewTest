c9142a9ba2f61c91cfebb39b4e1f4de086d5bbcc
Chuck Dascalos <dascalos@hotmail.com>
2013-04-22 15:34:52 -0400

    Initial Check-In

---
+++ BeerViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TabListViewTest
+{
+	public class BeerViewModel : BaseViewModel
+	{
+		public BeerViewModel()
+		{
+			BeerList.Add(new Beer
+				{
+					BeerName = "Founders KBS",
+					BeerCharacteristics = new List<BeerCharacteristic>
+						{
+							new BeerCharacteristic { Characteristic = "Hops", Level = "High" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "Low"},
+							new BeerCharacteristic { Characteristic = "Hops", Level = "High" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "Low"},
+							new BeerCharacteristic { Characteristic = "Hops", Level = "High" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "Low"},
+							new BeerCharacteristic { Characteristic = "Hops", Level = "High" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "Low"},
+							new BeerCharacteristic { Characteristic = "Hops", Level = "High" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "Low"}
+						}
+				});
+			BeerList.Add(new Beer
+			{
+				BeerName = "Goose Island IPA",
+				BeerCharacteristics = new List<BeerCharacteristic>
+						{
+							new BeerCharacteristic { Characteristic = "Hops", Level = "Low" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "High"}
+						}
+			});
+			BeerList.Add(new Beer
+			{
+				BeerName = "Leinenkugel Dark",
+				BeerCharacteristics = new List<BeerCharacteristic>
+						{
+							new BeerCharacteristic { Characteristic = "Hops", Level = "Medium" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Low"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "High"}
+						}
+			});
+			BeerList.Add(new Beer
+			{
+				BeerName = "Magic Hat Vinyl",
+				BeerCharacteristics = new List<BeerCharacteristic>
+						{
+							new BeerCharacteristic { Characteristic = "Hops", Level = "High" },
+							new BeerCharacteristic { Characteristic = "Barley", Level = "Medium"},
+							new BeerCharacteristic { Characteristic = "Grain", Level = "Low"}
+						}
+			});
+		}
+
+		private readonly ObservableCollection<Beer> _beerList = new ObservableCollection<Beer>();
+		public ObservableCollection<Beer> BeerList
+		{
+			get { return _beerList; }
+		}
+	}
+
+	public class Beer
+	{
+		public string BeerName { get; set; }
+		public List<BeerCharacteristic> BeerCharacteristics { get; set; }
+	}
+
+	public class BeerCharacteristic
+	{
+		public string Characteristic { get; set; }
+		public string Level { get; set; }
+	}
+
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace TabListViewTest
+{
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = new BeerViewModel();
+		}
+
+		private void BeerDetailsListView_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			Console.WriteLine("List View Loaded: {0}", sender);
+		}
+
+		private void Selector_OnSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			Console.WriteLine("Tab Selection Changed {0}", e.AddedItems[0]);
+		}
+	}
+}
